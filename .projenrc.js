fa3727e8a830a72b4d2b34c9ff1dac5e69a3a6c3
Peter Van Bouwel <pbbouwel@amazon.com>
2021-04-07 09:09:51 +0200

    feat: Only set release workflow for release branch
    When pushing changes to main we don't release a new version yet. We do want
    to release a new version whenever the release branch is updated. The version
    that we use to release artifacts should always match the CDK version or be higher in minor version numbering.

--- .projenrc.js
+++ .projenrc.js
@@ -56,9 +56,149 @@ const project = new AwsCdkConstructLibrary({
     module: 'cdk_stepfunctions_redshift',
     distName: PROJECT_NAME,
   },
+  releaseWorkflow: false, // Will have custom release workflow
 });
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'docker-compose.yml', 'yarn-error.log', '**/.idea', 'package-lock.json'];
 project.npmignore.exclude(...common_exclude, '/codebase');
 project.gitignore.exclude(...common_exclude);
+const isCdkVersionDefinedAsExpected = 'grep "^const AWS_CDK_VERSION =" .projenrc.js | wc -l | grep 1 >/dev/null';
+const cdkVerMatch = project.addTask('cdk-ver-match', { condition: isCdkVersionDefinedAsExpected } );
+cdkVerMatch.exec(' ' +
+    '  grep \'"version": "\'"$(grep "^const AWS_CDK_VERSION" .projenrc.js | grep -o "[0-9]*\\.[0-9]*\\.")[0-9][0-9]*"\'"\' version.json 2>/dev/null' +
+    '  || standard-version --release-as "$(grep "^const AWS_CDK_VERSION" .projenrc.js | grep -o "[0-9]*\\.[0-9]*\\.[0-9]*")"');
+
+releaseWorkflow = project.github.addWorkflow('releaseWorkflow');
+releaseWorkflow.on({
+  push: { branches: ['release'] },
+  workflow_dispatch: {},
+});
+releaseWorkflow.addJobs({
+  build: {
+    'runs-on': 'ubuntu-latest',
+    'env': {
+      CI: 'true',
+    },
+    'steps': [
+      {
+        name: 'Checkout',
+        uses: 'actions/checkout@v2',
+        with: {
+          'fetch-depth': 0,
+        },
+      },
+      {
+        name: 'Install dependencies',
+        run: 'yarn install --check-files --frozen-lockfile',
+      },
+      {
+        name: 'Synthesize project files',
+        run: 'npx projen',
+      },
+      {
+        name: 'Anti-tamper check',
+        run: 'git diff --exit-code',
+      },
+      {
+        name: 'Set git identity',
+        run: 'git config user.name "Auto-bump" && git config user.email "github-actions@github.com"',
+      },
+      {
+        name: 'Make sure CDK major/minor version is matched, patch version can be different',
+        run: 'npx projen cdk-ver-match',
+      },
+      {
+        name: 'Bump to next version',
+        run: 'npx projen bump',
+      },
+      {
+        name: 'Build',
+        run: 'npx projen build',
+      },
+      {
+        name: 'Anti-tamper check',
+        run: 'git diff --exit-code',
+      },
+      {
+        name: 'Push commits',
+        run: 'git push origin $BRANCH',
+        env: {
+          BRANCH: '${{ github.ref }}',
+        },
+      },
+      {
+        name: 'Push tags',
+        run: 'git push --follow-tags origin $BRANCH',
+        env: {
+          BRANCH: '${{ github.ref }}',
+        },
+      },
+      {
+        name: 'Upload artifact',
+        uses: 'actions/upload-artifact@v2.1.1',
+        with: {
+          name: 'dist',
+          path: 'dist',
+        },
+      },
+    ],
+    'container': {
+      image: 'jsii/superchain',
+    },
+  },
+  release_npm: {
+    'name': 'Release to NPM',
+    'needs': 'build',
+    'runs-on': 'ubuntu-latest',
+    'container': {
+      image: 'jsii/superchain',
+    },
+    'steps': [
+      {
+        name: 'Download build artifacts',
+        uses: 'actions/download-artifact@v2',
+        with: {
+          name: 'dist',
+          path: 'dist',
+        },
+      },
+      {
+        name: 'Release',
+        run: 'npx -p jsii-release@latest jsii-release-npm',
+        env: {
+          NPM_TOKEN: '${{ secrets.NPM_TOKEN }}',
+          NPM_DIST_TAG: 'latest',
+          NPM_REGISTRY: 'registry.npmjs.org',
+        },
+      },
+    ],
+  },
+  release_pypi: {
+    'name': 'Release to PyPi',
+    'needs': 'build',
+    'runs-on': 'ubuntu-latest',
+    'container': {
+      image: 'jsii/superchain',
+    },
+    'steps': [
+      {
+        name: 'Download build artifacts',
+        uses: 'actions/download-artifact@v2',
+        with: {
+          name: 'dist',
+          path: 'dist',
+        },
+      },
+      {
+        name: 'Release',
+        run: 'npx -p jsii-release@latest jsii-release-pypi',
+        env: {
+          TWINE_USERNAME: '${{ secrets.TWINE_USERNAME }}',
+          TWINE_PASSWORD: '${{ secrets.TWINE_PASSWORD }}',
+        },
+      },
+    ],
+  },
+});
+
 project.synth();
